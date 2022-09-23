45991d6c6869b70e55046a4bc69e2d9bbae6f232
nitesh89 <nitesh.khatri88@gmail.com>
2022-09-23 19:32:43 +0530

    changed

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
 
-	// megamenu 
+	// megamenu and single dropdown for desktop device
 	$(function(){
-		$(".navbar-nav .dropdown_megamenu").hover(function(){
-			$(".list_megamenu").toggleClass('list_megamenu_active')
-		});
-		$(".navbar-nav .dropdown").hover(function(){
-			$(".list_singledropdown").toggleClass('list_singledropdown_active')
-		});
+		  if ($(window).width() > 992 ){
+			$(".navbar-nav .dropdown_megamenu").hover(function(){
+				$(".list_megamenu").toggleClass('list_megamenu_active')
+			});
+			$(".navbar-nav .dropdown").hover(function(){
+				$(".list_singledropdown").toggleClass('list_singledropdown_active')
+			});
+		}
 	});
+
+	// megamenu and single dropdown for tablet and mobile device
+	$(function(){
+		if ($(window).width() < 991 ){
+			$(".navbar-nav .dropdown_megamenu").click(function(){
+				$(".list_megamenu").slideToggle(500)
+				$(".icon_arrow").toggleClass('icon_arrow_active')
+			});
+			$(".navbar-nav .dropdown").click(function(){
+				$(".list_singledropdown").slideToggle(500)
+				$(".icon_arrow").toggleClass('icon_arrow_active')
+			});
+		}
+	})
 	// SLIDESHOW 
     $(function() {
 	  // Owl Carousel
@@ -21,6 +37,8 @@ $(document).ready(function(){
 	      margin: 10,
 	      autoHeight: false,
 	      stagePadding: 50,
+	      autoplay:true,
+    	   autoplayTimeout:1000,
 	      responsive:{
 	        0:{
 	          items: 1,
